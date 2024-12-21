dba1767ba7b531ac6783a2d4313b5d7819904c4f
HetviSolanki048 <hetvisolanki048@gmail.com>
2024-12-21 10:28:10 +0530

    21-12-2024

---
+++ ADDITION.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int a,b,c;
+   clrscr();
+   printf("Enter a value of A : ");
+   scanf("%d",&a);
+   printf("Enter a value of B : ");
+   scanf("%d",&b);
+   c=a+b;
+   printf("Addition :%d",c);
+   getch();
+
+ }
